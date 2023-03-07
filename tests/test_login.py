35307d53d7f9be31338ff66e926cb26d1cfa9166
KokoNinja <negikonika1@gmail.com>
2023-03-07 10:05:04 +0530

    "added invalid login"

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,28 +1,29 @@
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
+from base.webdriver_listener import WebDriverWrapper
 
-class WebDriverWrapper:
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self):
-        serv_driver = Service(executable_path=r"C:\Users\JiDi\Downloads\chromedriver_win32 (5)\chromedriver.exe")
-        self.driver = webdriver.Chrome(service=serv_driver)
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(20)
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        yield
-        self.driver.quit()
 
 class TestLogin(WebDriverWrapper):
 
     def test_valid_login(self):
-        self.driver.find_element(By.NAME,"username").send_keys("Admin")
-        self.driver.find_element(By.NAME,"password").send_keys("admin123")
-        self.driver.find_element(By.XPATH,"//button[@type='submit']").click()
-        actual_text=self.driver.find_element(By.XPATH,"//h6[normalize-space()='Dashboard']").text
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
         assert_that("Dashboard").is_equal_to(actual_text)
+
+    def test_invalid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("admin")
+        self.driver.find_element(By.NAME, "password").send_keys("pass")
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        actual_text1 = self.driver.find_element(By.XPATH, "//p[contains(normalize-space() = 'Invalid credentials')]").text
+        assert_that("Invalid credentials").is_equal_to(actual_text1)
+        time.sleep(5)
+
 
 
 
